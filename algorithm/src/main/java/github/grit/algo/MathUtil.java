7303df84fb753f824b6ffbffc3c8f19ea0eb8dd4
Moncoder <ljy200220192020@outlook.com>
2025-04-13 20:50:03 +0800

    feat: 快速幂模板

---
+++ algorithm/src/main/java/github/grit/algo/MathUtil.java
@@ -0,0 +1,24 @@
+package github.grit.algo;
+
+public class MathUtil {
+
+    /**
+     * 快速幂
+     *
+     * @param base     底数
+     * @param exponent 指数
+     * @return 计算结果
+     */
+    public static int quickPower(int base, int exponent) {
+        int result = 1;
+        while (exponent > 0) {
+            if ((exponent & 1) == 1) {
+                result *= base;
+            }
+            base *= base;
+            exponent >>= 1;
+        }
+        return result;
+    }
+
+}
